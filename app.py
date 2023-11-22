3db4c61dd476200252b99a25ff4e7f43a55a0ac5
Fredrik Angelsen <fredrikangelsen@gmail.com>
2023-11-22 10:43:28 +0100

    Now using picamera2

--- app.py
+++ app.py
@@ -1,46 +1,56 @@
 from flask import Flask, Response, render_template
-from picamera2 import Camera
 import cv2
 from pyzbar.pyzbar import decode
+from picamera2 import Picamera2
 
 app = Flask(__name__)
+picam2 = None
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/start_camera')
+def start_camera():
+    global picam2
+    picam2 = Picamera2()
+    picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+    picam2.start()
+    return '', 204
+
+@app.route('/stop_camera')
+def stop_camera():
+    global picam2
+    if picam2:
+        picam2.stop()
+        picam2 = None
+    return '', 204
+
 @app.route('/video_feed')
 def video_feed():
+    
     def gen_frames():
-        camera = Camera()
-        camera.start_streaming()
-
+        global picam2
         while True:
-            # Retrieve the current frame
-            stream = camera.get_latest_stream()
-            frame = stream.get_frame()
+            if picam2:
+                frame = picam2.capture_array()
+                if frame is not None:
+                    # Your barcode decoding and frame manipulation code here
+                    for d in decode(frame):
+                        s = d.data.decode()
+                        print(s)
+                        frame = cv2.rectangle(frame, (d.rect.left, d.rect.top),
+                                              (d.rect.left + d.rect.width, d.rect.top + d.rect.height), (0, 255, 0), 3)
+                        frame = cv2.putText(frame, s, (d.rect.left, d.rect.top + d.rect.height),
+                                            cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2, cv2.LINE_AA)
 
-            # Convert the frame to RGB format
-            frame = frame.convert('RGB')
-
-            # Decode barcodes in the frame
-            for d in decode(frame):
-                s = d.data.decode()
-                print(s)
-
-                # Draw a rectangle around the barcode
-                cv2.rectangle(frame, (d.rect.left, d.rect.top),
-                                    (d.rect.left + d.rect.width, d.rect.top + d.rect.height), (0, 255, 0), 3)
-
-                # Write the barcode data on the frame
-                cv2.putText(frame, s, (d.rect.left, d.rect.top + d.rect.height), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2, cv2.LINE_AA)
-
-            # Encode the modified frame as JPEG
-            encoded_frame = cv2.imencode('.jpg', frame)[1].tobytes()
-
-            yield b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + encoded_frame + b'\r\n\r\n'
-
-        #camera.stop_streaming()
+                    encoded_frame = cv2.imencode('.jpg', frame)[1].tobytes()
+                    yield b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + encoded_frame + b'\r\n\r\n'
+                else:
+                    print('Error capturing frame')
+                    break
+            else:
+                break  # Exit the loop if the camera is not started
 
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
